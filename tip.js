e18948bdb99e02767e7d65c561c9c4ae650d4108
eladcrock <71683815+eladcrock@users.noreply.github.com>
2024-06-21 00:16:43 -0700

    Update tip.js
    ktips

--- tip.js
+++ tip.js
@@ -1,9 +1,9 @@
 function calculateTip() {
-    const gratuity = parseFloat(document.getElementById('gratuity').value) || 0;
-    const sales = parseFloat(document.getElementById('sales').value) || 0;
-    const wine = parseFloat(document.getElementById('wine').value) || 0;
-    const host_total = parseInt(document.getElementById('host_total').value) || 0;
-    const runner_total = parseInt(document.getElementById('runner_total').value) || 0;
+    const totalTips = parseFloat(document.getElementById('totalTips').value) || 0;
+    const netSales = parseFloat(document.getElementById('netSales').value) || 0;
+    const wineSales = parseFloat(document.getElementById('wineSales').value) || 0;
+    const numHosts = parseInt(document.getElementById('numHosts').value) || 0;
+    const numRunners = parseInt(document.getElementById('numRunners').value) || 0;
 
     const BUSSER_PERCENT = 0.16;
     const PORTER_PERCENT = 0.01;
@@ -11,55 +11,62 @@ function calculateTip() {
     const DUO_PERCENT = 0.07;
     const TRIO_PERCENT = 0.09;
     const WINE_PERCENT = 0.03;
+    const KITCHEN_TIP = 5;
 
     let buffTip = 0, hostTip = 0, barTip = 0, totalTip = 0,
-        bussTip, sommTip = 0, runTip = 0;
+        bussTip, sommTip = 0, runTip = 0, kitchenTip = 0;
 
-    if (sales < 2500) {
-        buffTip = sales * PORTER_PERCENT;
-    } else if (sales >= 2500) {
+    if (netSales < 2500) {
+        buffTip = netSales * PORTER_PERCENT;
+    } else {
         buffTip = 25;
     }
 
-    if (wine > 0) {
-        sommTip = wine * WINE_PERCENT;
+    if (wineSales > 0) {
+        sommTip = wineSales * WINE_PERCENT;
     }
 
-    if (runner_total === 3) {
-        runTip = gratuity * TRIO_PERCENT;
-    } else if (runner_total === 2) {
-        runTip = gratuity * DUO_PERCENT;
-    } else if (runner_total === 1) {
-        runTip = gratuity * SOLO_PERCENT;
+    if (numRunners === 3) {
+        runTip = totalTips * TRIO_PERCENT;
+    } else if (numRunners === 2) {
+        runTip = totalTips * DUO_PERCENT;
+    } else if (numRunners === 1) {
+        runTip = totalTips * SOLO_PERCENT;
     }
 
-    if (host_total === 3) {
-        hostTip = gratuity * TRIO_PERCENT;
-    } else if (host_total === 2) {
-        hostTip = gratuity * DUO_PERCENT;
-    } else if (host_total === 1) {
-        hostTip = gratuity * SOLO_PERCENT;
+    if (numHosts === 3) {
+        hostTip = totalTips * TRIO_PERCENT;
+    } else if (numHosts === 2) {
+        hostTip = totalTips * DUO_PERCENT;
+    } else if (numHosts === 1) {
+        hostTip = totalTips * SOLO_PERCENT;
     }
 
-    bussTip = gratuity * BUSSER_PERCENT;
-    barTip = gratuity * DUO_PERCENT;
+    bussTip = totalTips * BUSSER_PERCENT;
+    barTip = totalTips * DUO_PERCENT;
 
-    const netTip = gratuity - bussTip - sommTip - hostTip - runTip - buffTip - barTip;
-    totalTip = gratuity - netTip;
+    if (totalTips > 0) {
+        kitchenTip = KITCHEN_TIP;
+    }
+
+    const netTip = totalTips - bussTip - sommTip - hostTip - runTip - buffTip - barTip - kitchenTip;
+    totalTip = totalTips - netTip;
 
     document.getElementById('output').innerHTML = `
-        <p>Based on Values Provided</p>
+        <p>Based on a NET of $${netSales.toFixed(2)}</p>
+        <p>Wine sales of $${wineSales.toFixed(2)},</p>
+        <p>with ${numHosts} Hosts, ${numRunners} Runners,</p>
         <p>You will tip-out:</p>
         <p>$${bussTip.toFixed(2)} to Busser</p>
         <p>$${sommTip.toFixed(2)} to Somm</p>
         <p>$${buffTip.toFixed(2)} to Porter</p>
-        <p>$${runTip.toFixed(2)} to Runner</p>
+        <p>$${runTip.toFixed(2)} to Runner(s)</p>
         <p>$${barTip.toFixed(2)} to Bar</p>
-        <p>$${hostTip.toFixed(2)} to Host</p>
+        <p>$${hostTip.toFixed(2)} to Host(s)</p>
+        <p>$${kitchenTip.toFixed(2)} to Kitchen</p>
         <p>Server tips: $${netTip.toFixed(2)}</p>
         <p>Total tipout: $${totalTip.toFixed(2)}</p>
-        <p></p>
-            <p>Keep up the great work!</p>
-
     `;
+
+    document.getElementById('results').classList.remove('hidden');
 }
